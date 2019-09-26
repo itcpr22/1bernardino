a4665439de6ec08bd1071b91b261bed85ef0c268
Marie Sheena <Marie Sheena@LAPTOP-RS8CFBOL>
2019-09-26 01:18:56 -0800

    add product

---
+++ src/login_class.java
@@ -0,0 +1,60 @@
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marie Sheena
+ */
+public class login_class {
+    connection con = new connection();
+    
+    String fname = "";
+    
+    public int login(String username, String pass){
+        int y = 0;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = (Connection) DriverManager.getConnection(con.url,con.username,con.password);
+            
+            String sql = "SELECT * FROM register WHERE username = ? AND pass= MD5(?);";
+            PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(sql);
+            
+            pstmt.setString(1, username);
+            pstmt.setString(2, pass);
+            
+            ResultSet rs = pstmt.executeQuery();
+            
+            if(rs.next()){
+                y = 1;
+                fname = rs.getString("firstname");
+            }else{
+                y = 0;
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(login_class.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(login_class.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return y;
+    }
+
+    
+
+    
+}
+
